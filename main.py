c9894776b49ed92ae878bd2b1cd00f08782b0ba0
UShin0 <118548274+UShin0@users.noreply.github.com>
2024-10-18 12:52:50 +0200

    Update main.py
    The basic thing is done

--- main.py
+++ main.py
@@ -1,13 +1,53 @@
 print("Please input the number")
-num = int(input())
+num = input()
 print("What system is the number from? d(decimal), b(binary), h(hex),o(octal)")
 system = input()
 original = num
 
+def toDecimal():
+    global num
+    dec = 0
+    if system == "h":
+        num = num.upper()
+        for i in range(len(num)):
+            temp = num[len(num)-(i+1)]
+            if temp == "A":
+                temp = 10
+            elif temp == "B":
+                temp = 11
+            elif temp == "C":
+                temp = 12
+            elif temp == "D":
+                temp = 13
+            elif temp == "E":
+                temp = 14
+            elif temp == "F":
+                temp = 15
+            dec = dec + int(temp)*(16**i)
+        num = dec
+        return num
+    elif system == "o":
+        if not num.isalnum():
+            print("That's not an Octal number")
+        for i in range(len(num)):
+            temp = num[len(num)-(i+1)]
+            dec = dec + int(temp)*(8**i)
+        num = dec
+        return num
+    elif system == "b":
+        for i in range(len(num)):
+            temp = num[len(num)-(i+1)]
+            temp = int(temp)
+            if temp > 1 or temp < 0:
+                print("That´s not a binary number")
+            dec = dec + int(temp)*(2**i)
+        num = dec
+        return num
+
 def hex():              ##Need a decimal number
     global h
     global num
-    temp_num = num
+    temp_num = int(num)
     h = ""
     while True:
         i = temp_num % 16
@@ -31,7 +71,7 @@ def hex():              ##Need a decimal number
 def octal():                ##Need a decimal as well. So, i need a toDecimal() before calling those
     global o
     global num
-    temp_num = num
+    temp_num = int(num)
     o = ""
     while True:
         i = temp_num % 8
@@ -42,7 +82,7 @@ def octal():                ##Need a decimal as well. So, i need a toDecimal() b
 def binary():               ##Same decimal thing
     global b
     global num
-    temp_num = num
+    temp_num = int(num)
     b = ""
     while True:
         i = temp_num % 2
@@ -56,3 +96,21 @@ if system =="d":
     octal()
     binary()
     print(f"Decimal: {original} \nHexadecimal: {h} \nOctal: {o} \nBinary: {b}")
+elif system == "h":
+    toDecimal()
+    octal()
+    binary()
+    original = original.upper()
+    print(f"Decimal: {num} \nHexadecimal: {original} \nOctal: {o} \nBinary: {b}")
+elif system == "o":
+    toDecimal()
+    hex()
+    binary()
+    print(f"Decimal: {num} \nHexadecimal: {h} \nOctal: {original} \nBinary: {b}")
+elif system == "b":
+    toDecimal()
+    hex()
+    octal()
+    print(f"Decimal: {num} \nHexadecimal: {h} \nOctal: {o} \nBinary: {original}")
+else:
+    print("Error, Input not recognized")
